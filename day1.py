723fe62a218fd53f0d274447796b1ce1c1a02e01
Ceesjan Luiten <github.com@qtea.nl>
2020-12-05 12:43:18 +0100

    Error handling

--- day1.py
+++ day1.py
@@ -1,13 +1,21 @@
 #!/bin/bash
 import sys
-found = [False] * 2200
-with open(sys.argv[1]) as handle:
-    for line in handle:
-        number = int(line)
-        opposite_number = 2020 - number
-        if found[opposite_number]:
-            print("{} * {} = {}".format(opposite_number, number, opposite_number * number))
-            sys.exit(0)
-        found[number] = True
-print("No combination found that adds up to 2020.")
-sys.exit(1)
+
+def main():
+    desired_sum = 2200
+    found = [False] * desired_sum
+    with open(sys.argv[1]) as handle:
+        for line in handle:
+            found_number = int(line)
+            if found_number <= 0 or found_number >= desired_sum:
+                continue
+            opposite_number = desired_sum - found_number
+            if found[opposite_number]:
+                print("{} * {} = {}".format(opposite_number, number, opposite_number * number))
+                sys.exit(0)
+            found[found_number] = True
+    print("No combination found that adds up to 2020.")
+    sys.exit(1)
+
+if __name__ == '__main__':
+    main()
